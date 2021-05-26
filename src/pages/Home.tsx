b99e58cd612d52707065aee91f21045f1cbed6d4
Gustavo <gustavoprizon@gmail.com>
2021-05-26 02:24:11 -0300

    refactor skillcard with typescript and finish app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,6 +28,10 @@ export function Home() {
     setMySkills((oldState) => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
 
@@ -55,7 +59,12 @@ export function Home() {
       <FlatList
         data={mySkills}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SkillCard skill={item.name} />}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
       />
     </View>
   );
