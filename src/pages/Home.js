77a82318e19d56847452ac7c16f6bb4d25b02d3c
Gustavo <gustavoprizon@gmail.com>
2021-05-26 01:01:25 -0300

    implement gretting with useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -13,14 +13,28 @@ import { SkillCard } from "../components/SkillCard";
 export function Home() {
   const [newSkill, setNewSkill] = useState("");
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState("");
 
   function handleAddNewSkill() {
     setMySkills((oldState) => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGretting("Good morning!");
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting("godd afternoon!");
+    } else {
+      setGretting("Good night!");
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Gustavo</Text>
+      <Text style={styles.grettings}>{gretting}</Text>
       <TextInput
         style={styles.input}
         placeholder="My Skill"
@@ -57,5 +71,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === "ios" ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  grettings: {
+    color: "#fff",
   },
 });
